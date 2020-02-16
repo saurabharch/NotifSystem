4a0ac8dcc254bc4157e565035b0aac67be8a04fd
Jin <it.minhhung@gmail.com>
2020-02-16 19:14:31 +0700

    init signalr with demo simple chat

---
+++ NotifSystem/NotifSystem/NotifSystem.Web/App_Start/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NotifSystem.App_Start.Startup))]
+namespace NotifSystem.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+
+            var hubConfiguration = new HubConfiguration {EnableDetailedErrors = HttpContext.Current.IsDebuggingEnabled};
+
+            app.MapSignalR(hubConfiguration);
+
+            //GlobalHost.Configuration.ConnectionTimeout = TimeSpan.FromSeconds(50);
+        }
+    }
+}
---
+++ NotifSystem/NotifSystem/NotifSystem.Web/Modules/Default/SimpleChat/SimpleChatPage.cs
@@ -0,0 +1,19 @@
+﻿namespace NotifSystem.Default.Pages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    [RoutePrefix("Default/SimpleChat"), Route("{action=index}")]
+    [Authorize]
+    public class SimpleChatController : Controller
+    {
+        // GET: SimpleChat
+        public ActionResult Index()
+        {
+            return View("~/Modules/Default/SimpleChat/SimpleChatIndex.cshtml");
+        }
+    }
+}
